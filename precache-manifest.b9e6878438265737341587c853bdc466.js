4ad386d1fef94d42c5d26e235156acc5a9825907
Anupam Sharma <anupamamity1995@gmail.com>
2020-07-19 20:06:32 +0530

    Updates

---
+++ precache-manifest.b9e6878438265737341587c853bdc466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1fe804e77df4078e65941ec0a3ae0b",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "2499b51bdf8d08235522",
+    "url": "/deploy-react/static/css/main.d6daa60f.chunk.css"
+  },
+  {
+    "revision": "9797a24ce4c45584ca93",
+    "url": "/deploy-react/static/js/2.5c09c3c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.5c09c3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2499b51bdf8d08235522",
+    "url": "/deploy-react/static/js/main.ac57c680.chunk.js"
+  },
+  {
+    "revision": "f878444a6e1b8c5c8beb",
+    "url": "/deploy-react/static/js/runtime-main.33cbb215.js"
+  }
+]);
